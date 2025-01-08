23ad4f7f268a09d538c659b94422f62d398d76d6
Michal <mwargan@gmail.com>
2025-01-08 18:32:58 +0200

    Add country support in edge functions and update country pages with city listings

--- netlify/edge-functions/rewriteCountries.ts
+++ netlify/edge-functions/rewriteCountries.ts
@@ -1,5 +1,12 @@
+import { Country } from "country-state-city";
+
 export default async (request, context) => {
-  const path = "/countries/" + context.geo?.country?.name.toLowerCase();
+  const geoCountryCode = context.geo?.country?.code;
+
+  const country = Country.getCountryByCode(geoCountryCode);
+
+  const path = "/countries/" + country?.name.toLowerCase();
+
   return Response.redirect(new URL(path, request.url));
 };
 
